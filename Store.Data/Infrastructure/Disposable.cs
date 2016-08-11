5f094dd28edcc569f21167829c9020c5e18b642d
Moshiour <bidrohipothik@gmail.com>
2016-08-11 18:06:41 +0600

    Infrastracture Disponse and IDbFactory

---
+++ Store.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Store.Data.Infrastructure
+{
+    class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        //this is the method that was implemeted to fulfil the interface requriement  another overlodading is supporting method to do this job
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
