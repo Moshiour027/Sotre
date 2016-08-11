be0965625adbc1bfafd040dc78799d9ff934100c
Moshiour <bidrohipothik@gmail.com>
2016-08-11 15:45:33 +0600

    Inintial Commit of Best Practices or Understanding patterns and Unit of work .

---
+++ Store.Model/Models/Category.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Store.Model.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+
+        public DateTime? DateUpdated { get; set; }
+
+        public virtual List<Gadget> Gedgets { get; set; }
+
+    }
+}
