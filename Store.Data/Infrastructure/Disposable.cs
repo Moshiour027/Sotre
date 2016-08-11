dbfff5892f3ce87acdf97494801724f8f1544e6d
Moshiour <bidrohipothik@gmail.com>
2016-08-11 18:14:17 +0600

    Full implemetaion of unit of work in this commit

--- Store.Data/Infrastructure/Disposable.cs
+++ Store.Data/Infrastructure/Disposable.cs
@@ -2,7 +2,7 @@
 
 namespace Store.Data.Infrastructure
 {
-    class Disposable : IDisposable
+    public class Disposable : IDisposable
     {
         private bool isDisposed;
 
@@ -26,9 +26,37 @@ namespace Store.Data.Infrastructure
             isDisposed = true;
         }
 
+        //Override this to dispose custom objects
+        /*
+            This DisposeCore virtual method  will make other classes inherit this one, to dispose their own objects int he way they need to.
+            Now add the implemetaion class ofthe IDbFactory interface
+         */
+
         protected virtual void DisposeCore()
         {
 
+        }
+    }
+
+
+    //DbFactory class already inherited by this class and Disposable class has implemented the method of IDisposable,
+    //already and IDbFactory aslo inherit the inteface quality from the IDisposable so 
+
+    //so inshort the extra method Init() if implemeted will make each of them happy
+    public class DbFactory : Disposable, IDbFactory
+    {
+        StoreEntities dbContext;
+
+
+
+        public StoreEntities Init()
+        {
+            return dbContext ?? (dbContext = new StoreEntities());
+        }
+
+        protected override void DisposeCore()
+        {
+            dbContext?.Dispose();
         }
     }
 }
